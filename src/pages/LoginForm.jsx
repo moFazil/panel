781a57671e5e96d72a4da118002e21b2e0f220b2
Ahamed <ahamed.irfan001@gmail.com>
2025-03-05 14:28:08 +0530

    Update LoginForm.jsx

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,26 +1,45 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet } from "react-router-dom";
 
 const LoginForm = () => {
-
   return (
-    <div className='lg:w-[30%] md:w-[50%] sm:w-[70%] w-[90%] pb-10 mt-12 m-auto shadow-xl rounded-md'>
-        <form>
+    <div className="flex items-center justify-center h-screen">
+      <div className="lg:w-[30%] md:w-[50%] sm:w-[70%] w-[90%] shadow-xl rounded-md text-center flex flex-col items-center justify-center p-10">
+        <form className="w-full flex flex-col items-center">
+          <p className="py-5 my-4 text-2xl font-bold">
+            YOUR ACCOUNT FOR <br /> EVERYTHING
+          </p>
 
-          <p className='py-5 my-4 text-2xl font-bold'>YOUR ACCOUNT FOR <br/> EVERYTHING</p>
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="my-2 px-3 py-2 w-full rounded-md border-slate-300 border-[1px]"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className=" my-2 px-3 py-2 w-full rounded-md border-slate-300 border-[1px]"
+          />
 
-          <input type="email" placeholder='Email Address' className='my-2 px-3 py-2 w-8/12 rounded-md border-slate-300 border-[1px]'/>
-          <input type="password" placeholder='Password' className=' my-2 px-3 py-2 w-8/12 rounded-md border-slate-300 border-[1px]'/>
-
-          {/* <p className='font-normal text-sm text-gray-400 py-2'>By logging in, you agree to Foot's <br/><span className=' underline decoration-1 cursor-pointer'>Privacy Policy</span> and <span className=' underline decoration-1 cursor-pointer'>Terms of Use</span>.</p> */}
-          
-          <button type='submit' className='mb-5 mt-2 px-3 py-2 w-8/12 font-semibold bg-black text-white rounded-md border-[2px] border-black'>SIGN IN</button>
-
-          <p className=' font-normal text-sm text-gray-400 py-2'>Not a Member? <Link to={'/joinus'}> <span className=' underline decoration-1 text-black cursor-pointer'>Join Us</span></Link>.</p>
-            
+          <button
+            type="submit"
+            className="mb-5 mt-2 px-3 py-2 w-full font-semibold bg-black text-white rounded-md border-[2px] border-black"
+          >
+            SIGN IN
+          </button>
+{/* 
+          <p className="font-normal text-sm text-gray-400 py-2">
+            Not a Member?{" "}
+            <Link to={"/joinus"}>
+              <span className="underline decoration-1 text-black cursor-pointer">
+                Join Us
+              </span>
+            </Link>
+            .
+          </p> */}
         </form>
-      <Outlet/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
